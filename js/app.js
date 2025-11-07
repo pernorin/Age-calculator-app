c5da72563543b9c93244365aa8566e285d98f639
Per Norin <pelle.norin.79@gmail.com>
2025-11-07 15:20:06 +0100

    Reset JS

--- js/app.js
+++ js/app.js
@@ -5,38 +5,13 @@ const daysOutput = document.getElementById('days-output');
 
 dateForm.addEventListener('submit', handleSubmit);
 
-class Day {
-  constructor(day, month, year) {
-    this.day = day;
-    this.month = month;
-    this.year = year;
-  }
-}
-
-const now = new Date(Date.now());
-console.log(now.getDate(), now.getMonth() + 1, now.getFullYear(), typeof now);
-
-const today = new Day(now.getDate(), now.getMonth() + 1, now.getFullYear());
-console.log(today);
+const now = new Date();
+console.log(now.getDate(), now.getMonth() + 1, now.getFullYear());
 
 function handleSubmit(event) {
   event.preventDefault();
 
   const formData = new FormData(dateForm);
   const res = Object.fromEntries(formData);
-
-  const birthday = new Day(
-    parseInt(res.day),
-    parseInt(res.month),
-    parseInt(res.year)
-  );
-
   console.log(res);
-  console.log(birthday);
 }
-
-/*
-https://github.com/pernorin/frontend-assignment2/blob/master/index.html
-https://github.com/pernorin/frontend-assignment2/blob/master/script.js
-
-*/
